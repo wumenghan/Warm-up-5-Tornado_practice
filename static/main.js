bbdcb888cd7ed8ea8846251dbd90347dcdd28868
wumenghan <wu784@purdue.edu>
2015-05-02 15:20:02 -0400

    complete draft function, compare&add message

--- static/main.js
+++ static/main.js
@@ -1,12 +1,22 @@
 // Let user to be either A or B
 var player_list = ['A','B'];
-var play = player_list[Math.floor.(Math.random() * player_list.length)];
+//var play = player_list[Math.floor.(Math.random() * player_list.length)];
 // Global variable
 var g_a_seen = 0;
 var g_b_seen = 0;
 var g_correct = 0;
-$(document).ready(function(){
-	
+jQuery(document).ready(function(){
+	$("#message_a").on("keypress",A_player_handler);
+	$("#message_a").select();
+	$("#message_b").on("keypress",B_player_handler);
+	$("#message_a").on("keypress",CompareHandler);
+	$("#message_b").on("keypress",CompareHandler);
+
+	poll_A();
+	poll_B();
+	CompareHandler();
+	poll_compare();
+
 
 });
 
@@ -14,7 +24,7 @@ function A_player_handler(evt){
 	
 		if(evt.which == 13){
 			evt.preventDefault();
-		var message_input = document.getElementById("message_a")
+		var message_input = document.getElementById("message_a");
 		message_input.disabled = true;
 		$.ajax({
 			url:"/new_a",
@@ -23,8 +33,8 @@ function A_player_handler(evt){
 			success:function(data){
 				message_input.value = "";
 				message_input.disabled = false;
-				messgae_input.select();
-				},		
+				message_input.select();
+				}		
 			});
 		}
 }
@@ -38,8 +48,8 @@ function poll_A(){
 			success:function(data){
 			//Display message in browser
 			var html_parts = [];
-			for (var i = 0; i < data.messages.length;i++){
-					var text = data.messages[i];
+			for (var i = 0; i < data.messages_a.length;i++){
+					var text = data.messages_a[i];
 					var html = text.replace("&","&amp").replace("<","&lt;").replace(">","&gt;");
 					html = '<div class=a_message>'+html+'</div>';
 					html_parts.push(html);
@@ -48,8 +58,8 @@ function poll_A(){
 				document.getElementById("message_a_display").innerHTML = html_parts.join("");
 
 			
-			g_a_seen = data.messages.length;
-			poll_B();
+				g_a_seen = data.messages_a.length;
+				poll_A();
 			}
 		});
 }
@@ -58,7 +68,7 @@ function B_player_handler(evt){
 	
 		if(evt.which == 13){
 			evt.preventDefault();
-		var message_input = document.getElementById("message_b")
+		var message_input = document.getElementById("message_b");
 		message_input.disabled = true;
 		$.ajax({
 			url:"/new_b",
@@ -67,8 +77,8 @@ function B_player_handler(evt){
 			success:function(data){
 				message_input.value = "";
 				message_input.disabled = false;
-				messgae_input.select();
-				},		
+				message_input.select();
+				}		
 			});
 		}
 }
@@ -78,21 +88,21 @@ function poll_B(){
 		$.ajax({
 			url:"/update_b",
 			type:"POST",
-			data:{num_seen:g_a_seen},
+			data:{num_seen:g_b_seen},
 			success:function(data){
 			//Display message in browser
 			var html_parts = [];
-			for (var i = 0; i < data.messages.length;i++){
-					var text = data.messages[i];
+			for (var i = 0; i < data.messages_b.length;i++){
+					var text = data.messages_b[i];
 					var html = text.replace("&","&amp").replace("<","&lt;").replace(">","&gt;");
-					html = '<div class=a_message>'+html+'</div>';
+					html = '<div class=b_message>'+html+'</div>';
 					html_parts.push(html);
 					
 				}
-				document.getElementById("message_a_display").innerHTML = html_parts.join("");
+				document.getElementById("message_b_display").innerHTML = html_parts.join("");
 
 			
-			g_a_seen = data.messages.length;
+			g_b_seen = data.messages_b.length;
 			poll_B();
 			}
 		});
@@ -114,9 +124,12 @@ function poll_compare(){
 		
 		$.ajax({
 			url:"/update_compare",
+			type:"POST",
+			data:{correct:g_correct},
 			success:function(data){
 			//print data to brower
-			document.getElementById("correct_value").value = data;
+			document.getElementById("correct_value").value = data.correct;
+			g_correct = data.correct;
 			poll_compare();
 			}
 
